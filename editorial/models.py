402e347b79180dc00e15d73bb24aeda223e59f85
OmarGonD <oma.gonzales@gmail.com>
2019-06-14 13:15:25 -0500

    third commit

--- editorial/models.py
+++ editorial/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Editorial(models.Model):
     date = models.DateField()
-    title = models.CharField(max_length=40)
-    body = models.CharField(max_length=100)
-    url = models.CharField(max_length=400)
-    image = models.CharField(max_length=400)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=400)
+    url = models.CharField(max_length=600)
+    image = models.CharField(max_length=600)
 
     def __str__(self):
         return f"Editorial con id:  #{self.id}"
